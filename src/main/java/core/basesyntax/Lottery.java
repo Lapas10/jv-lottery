f8909bc91127449f24c8cd5951e04f040f1f0576
Alex <lapas777666@gmail.com>
2026-01-07 22:06:10 +0200

    i did ColorSupplier private

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX = 100;
     private static final Random RANDOM = new Random();
+    private static final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ColorSupplier supplier = new ColorSupplier();
 
         ball.setColor(supplier.getRandomColor());
         ball.setNumber(RANDOM.nextInt(MAX) + 1);
